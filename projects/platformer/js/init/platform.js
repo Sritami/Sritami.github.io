04d93c45b518a91da7cf3731d7bbe5e14954de03
Srija <srijatami@gmail.com>
2021-06-10 17:05:25 +0000

    update on website

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -39,6 +39,7 @@
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
+        createPlatform(200, 260, 0.3);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
