ba5f2cd679cce01342170324490116e4aacfd651
Srija <84638584+Sritami@users.noreply.github.com>
2021-09-16 18:13:39 +0000

    added nav bar + scroll bar

---
+++ index.js
@@ -0,0 +1,48 @@
+// maybe will have to code in javascript when clicked on menu, the nav bar will show up
+
+// var container = $('hr');
+// var menu = $('.menu-bar')
+
+
+// container.on("click", changeNavBar);
+
+// function changeNavBar () {
+//   container.css('visibility', 'invisible');
+
+// }
+
+
+// TESTING
+// var name = $("h1");
+// var btn = $("button");
+
+// btn.on("click", changeTitle);
+
+// function changeTitle() {
+//   name.text("testing");
+//   btn.toggleClass('toggle');
+//   console.log(name);
+// }
+
+
+
+
+
+
+// no errors but menu bar not visible after click
+//useCapture registering as false 
+// var button = $('.nav');
+// var barMenu = $(".menu-bar");
+
+
+// button.on("click", openMenuBar);
+// function openMenuBar() {
+//   // barMenu.css("visibility", "visible");
+//   barMenu.removeClass(".menu-bar");
+//   barmenu.addClass(".menu-baropened");
+// }
+
+
+
+
+
